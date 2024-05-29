3bcd9f89cea78a5da7406461780d923375ea9fb6
Xuyang Han <hanxuyang0826@gmail.com>
2024-05-28 20:31:51 -0400

    leetcode 1404

--- DataStructures/StringTests.cs
+++ DataStructures/StringTests.cs
@@ -516,6 +516,37 @@ namespace LeetCode.DataStructures
 
 
         #endregion
+
+        #region 1404. Number of Steps to Reduce a Number in Binary Representation to One
+        // Input: s = "1101"
+        // Output: 6
+        // Explanation: "1101" corressponds to number 13 in their decimal representation.
+        // Step 1) 13 is odd, add 1 and obtain 14.
+        // Step 2) 14 is even, divide by 2 and obtain 7.
+        // Step 3) 7 is odd, add 1 and obtain 8.
+        // Step 4) 8 is even, divide by 2 and obtain 4.
+        // Step 5) 4 is even, divide by 2 and obtain 2.
+        // Step 6) 2 is even, divide by 2 and obtain 1.
+        public int NumSteps(string s)
+        {
+            int steps = 0;
+            int carry = 0;
+            for (int i = s.Length - 1; i > 0; i--)
+            {
+                if (s[i] - '0' + carry == 1)
+                {
+                    steps += 2;
+                    carry = 1;
+                }
+                else
+                {
+                    steps += 1;
+                }
+            }
+
+            return steps + carry;
+        }
+        #endregion
     }
 }
 
